2889b4a8389cac8ded23d0ddbe4c20e8051a9c30
pakchu <gus4734@gmail.com>
2022-07-25 21:07:05 +0900

    bug or mistake?

--- assembly-line/src/lib.rs
+++ assembly-line/src/lib.rs
@@ -4,7 +4,7 @@
 
 pub fn production_rate_per_hour(speed: u8) -> f64 {
     match speed {
-        0..=4 => return speed as f64* 221 as f64,
+        0..=4 => return (speed * 221) as f64,
         5..=8 => return speed as f64 * 0.9 * 221 as f64,
         9..=10 => return speed as f64 * 0.77 * 221 as f64,
         _ => return 0.0,
@@ -14,7 +14,4 @@ pub fn production_rate_per_hour(speed: u8) -> f64 {
 pub fn working_items_per_minute(speed: u8) -> u32 {
     // unimplemented!("calculate the amount of working items at speed: {}", speed)
     (production_rate_per_hour(speed) / 60.0) as u32
-}
-fn main(){
-    println!("{}",production_rate_per_hour(4));
 }
